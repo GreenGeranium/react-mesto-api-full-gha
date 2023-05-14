5bfe1bd19e1837476e5e80f4a24af17e922b4f1a
Maxim Grassi <grassimacsim@yandex.ru>
2023-05-14 19:20:12 +0300

    feat: добавить новые ссылки на бэк 2

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -78,10 +78,11 @@ class Api {
   }
 }
 
+
 const api = new Api({
   baseUrl: "api.geranius.nomoredomains.monster",
   headers: {
-    Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDRmZDY4ODRlNWNlNGVlOWMxOGFiOGMiLCJpYXQiOjE2ODM5NjUxNjgsImV4cCI6MTY4NDU2OTk2OH0.WxpbfMpkT6993RKpMkBdaFheDS6sg7X2NOr2yEpCu-A",
+    "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDRmZDY4ODRlNWNlNGVlOWMxOGFiOGMiLCJpYXQiOjE2ODM5NjUxNjgsImV4cCI6MTY4NDU2OTk2OH0.WxpbfMpkT6993RKpMkBdaFheDS6sg7X2NOr2yEpCu-A",
     "Content-Type": "application/json",
   },
 });
